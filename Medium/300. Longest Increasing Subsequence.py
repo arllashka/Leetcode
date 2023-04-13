404a2074f93364918e47c7f28dd7640cf3d12b86
arllashka <arlan914@gmail.com>
2023-04-14 01:55:56 +0900

    300

---
+++ Medium/300. Longest Increasing Subsequence.py
@@ -0,0 +1,22 @@
+class Solution:
+    def lengthOfLIS(self, nums: List[int]) -> int:
+        
+        # dp = [1] * len(nums)
+
+        # for i in range(1, len(nums)):
+        #     for j in range(i):
+        #         if nums[i] > nums[j]:
+        #             dp[i] = max(dp[i], dp[j]+1)
+
+        # return max(dp)
+
+        sub = []
+
+        for num in nums:
+            i = bisect.bisect_left(sub, num)
+            if i == len(sub):
+                sub.append(num)
+            else:
+                sub[i] = num
+
+        return len(sub)  
