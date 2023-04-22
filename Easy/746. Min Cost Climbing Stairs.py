5551884b6cae70bbc1a05a6e1fedd41a630373a9
arllashka <arlan914@gmail.com>
2023-04-23 03:12:17 +0900

    dp top-down solution added to 746

--- Easy/746. Min Cost Climbing Stairs.py
+++ Easy/746. Min Cost Climbing Stairs.py
@@ -1,8 +1,15 @@
 class Solution:
-    def minCostClimbingStairs(self, cost: List[int]) -> int:
-        # complexity - O(n)
-        cost.append(0)
-
-        for i in range(len(cost)-3, -1, -1):
+   # dp solution added
+   # @cache
+        # def dp(i):
+        #     if i<2:
+        #         return cost[i]
+        #     return cost[i] + min(dp(i-1), dp(i-2))
+        # leng=len(cost)-1
+        # return min(dp(leng), dp(leng-1))
+        
+        # O(1) memory
+        for i in range (len(cost)-3, -1, -1):
             cost[i] += min(cost[i+1], cost[i+2])
+
         return min(cost[0], cost[1])
